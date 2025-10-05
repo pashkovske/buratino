2a76158298e056bbc7c92b1fa52b0876783e7fa0
pashkovske <pashkovske@gmail.com>
2025-10-05 21:24:05 +0300

    Add Instrument info cache

--- src/main/kotlin/ru/pashkovske/buratino/instrument/model/InstrumentId.kt
+++ src/main/kotlin/ru/pashkovske/buratino/instrument/model/InstrumentId.kt
@@ -2,4 +2,21 @@ package ru.pashkovske.buratino.instrument.model
 
 data class InstrumentId(
     val id: String
-)
+) {
+    override fun toString(): String {
+        return id
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as InstrumentId
+
+        return id == other.id
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+}
--- src/main/kotlin/ru/pashkovske/buratino/instrument/service/InstrumentServiceImpl.kt
+++ src/main/kotlin/ru/pashkovske/buratino/instrument/service/InstrumentServiceImpl.kt
@@ -4,16 +4,26 @@ import org.springframework.stereotype.Service
 import ru.pashkovske.buratino.instrument.adapter.InstrumentServiceAdapter
 import ru.pashkovske.buratino.instrument.model.Instrument
 import ru.pashkovske.buratino.instrument.model.InstrumentId
+import ru.pashkovske.buratino.instrument.repo.InstrumentRepo
+import java.time.Instant
 
 @Service
 class InstrumentServiceImpl(
-    private val instrumentServiceAdapter: InstrumentServiceAdapter
+    private val instrumentServiceAdapter: InstrumentServiceAdapter,
+    private val instrumentRepo: InstrumentRepo
 ) : InstrumentService {
     override fun getByTicker(ticker: String): Instrument {
         return instrumentServiceAdapter.getByTicker(ticker)
     }
 
     override fun get(iid: InstrumentId): Instrument {
-        return instrumentServiceAdapter.get(iid)
+        val lastUpdate = instrumentRepo.getLastUpdate(iid)
+        if (lastUpdate == null) {
+            instrumentRepo.create(instrumentServiceAdapter.get(iid))
+        }
+        else if (lastUpdate.plusSeconds(5).isBefore(Instant.now())) {
+            instrumentRepo.update(instrumentServiceAdapter.get(iid))
+        }
+        return instrumentRepo.get(iid)
     }
 }
