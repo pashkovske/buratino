e300d71ddcff40d196a1e9098a4cc03e21066ffe
pashkovske <pashkovske@gmail.com>
2025-11-09 22:50:56 +0300

    Docker scripts

---
+++ docker/push.sh
@@ -0,0 +1,2 @@
+docker tag buratino-trader-api cr.yandex/crp9v4cjakkbt4e1s2t0/buratino-trader-api:latest;
+docker push cr.yandex/crp9v4cjakkbt4e1s2t0/buratino-trader-api:latest
---
+++ docker/run.sh
@@ -0,0 +1,13 @@
+scp ../.env pashkovske078@158.160.203.148:/home/pashkovske078/buratino-trader-api/.env;
+ssh pashkovske078@158.160.203.148 << 'ENDSSH'
+  docker pull cr.yandex/crp9v4cjakkbt4e1s2t0/buratino-trader-api:latest;
+  cd /home/pashkovske078/buratino-trader-api;
+  docker stop buratino-trader-api;
+  docker rm buratino-trader-api;
+  docker run -d \
+      --name buratino-trader-api \
+      -p 8080:8080 \
+      --env-file .env \
+      cr.yandex/crp9v4cjakkbt4e1s2t0/buratino-trader-api:latest;
+  docker start buratino-trader-api
+ENDSSH
