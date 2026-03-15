1a87cfe30ae6254bfb895c2acfc10b0a77794c5e
pashkovske <pashkovske@gmail.com>
2026-03-15 17:41:02 +0300

    continueAssignment handle to abstract continue controller

--- src/main/kotlin/ru/pashkovske/buratino/assignment/parent/continuous/spread/fraction/controller/ContinuousFractionalSpreadAssignmentController.kt
+++ src/main/kotlin/ru/pashkovske/buratino/assignment/parent/continuous/spread/fraction/controller/ContinuousFractionalSpreadAssignmentController.kt
@@ -1,31 +1,37 @@
 package ru.pashkovske.buratino.assignment.parent.continuous.spread.fraction.controller
 
-import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import ru.pashkovske.buratino.assignment.base.controller.BasicAssignmentController
 import ru.pashkovske.buratino.assignment.base.dao.AssignmentDao
 import ru.pashkovske.buratino.assignment.base.model.scheduling.properties.PeriodicAssignmentSchedulingProperties
+import ru.pashkovske.buratino.assignment.limit.spread.fraction.controller.dto.FractionalSpreadAssignmentDto
+import ru.pashkovske.buratino.assignment.limit.spread.fraction.controller.dto.StartFractionalSpreadAssignmentDto
+import ru.pashkovske.buratino.assignment.limit.spread.fraction.model.FractionalSpreadAssignment
+import ru.pashkovske.buratino.assignment.parent.continuous.base.controller.ContinuousAssignmentController
 import ru.pashkovske.buratino.assignment.parent.continuous.spread.fraction.controller.dto.ContinuousFractionalSpreadAssignmentDto
 import ru.pashkovske.buratino.assignment.parent.continuous.spread.fraction.controller.mapper.ContinuousFractionalSpreadAssignmentMapper
 import ru.pashkovske.buratino.assignment.parent.continuous.spread.fraction.model.ContinuousFractionalSpreadAssignment
 import ru.pashkovske.buratino.assignment.parent.continuous.spread.fraction.model.ContinuousFractionalSpreadAssignmentStartCmd
 import ru.pashkovske.buratino.assignment.parent.continuous.spread.fraction.service.ContinuousFractionalSpreadAssignmentExe
-import ru.pashkovske.buratino.assignment.limit.spread.fraction.controller.dto.StartFractionalSpreadAssignmentDto
 import ru.pashkovske.buratino.instrument.model.InstrumentId
 import ru.pashkovske.buratino.order.model.OrderDirection
-import java.util.UUID
 
 @Suppress("unused")
 @RestController
 @RequestMapping("/assignment/continuous/fractional-spread")
 class ContinuousFractionalSpreadAssignmentController(
     dao: AssignmentDao<ContinuousFractionalSpreadAssignment>,
-    override val exe: ContinuousFractionalSpreadAssignmentExe
-): BasicAssignmentController<ContinuousFractionalSpreadAssignment, ContinuousFractionalSpreadAssignmentDto, ContinuousFractionalSpreadAssignmentStartCmd>(
+    exe: ContinuousFractionalSpreadAssignmentExe
+) : ContinuousAssignmentController<
+    ContinuousFractionalSpreadAssignment,
+    FractionalSpreadAssignment,
+    ContinuousFractionalSpreadAssignmentStartCmd,
+    ContinuousFractionalSpreadAssignmentDto,
+    FractionalSpreadAssignmentDto
+    >(
     dao = dao,
     exe = exe
 ) {
@@ -55,10 +61,5 @@ class ContinuousFractionalSpreadAssignmentController(
             }
         )
         return doStart(cmd)
-    }
-
-    @PatchMapping("/{id}/continue")
-    fun continueAssignment(@PathVariable id: UUID): ContinuousFractionalSpreadAssignmentDto {
-        return toDto(exe.continueAssignment(id))
     }
 }
